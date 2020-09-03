53c64364fd5d1326dd0bf3ec2593bb8abd327f8e
bassam-alamin <bassamalchinsky96@gmail.com>
2020-09-03 13:26:38 +0300

    changed columnName

--- education.sql
+++ education.sql
@@ -1,4 +1,4 @@
-SELECT institution.name AS institutionName,course.name,COUNT(institutionid) as NoOfStudents
+SELECT institution.name AS institutionName,course.name AS courseName,COUNT(institutionid) as NoOfStudents
 FROM institution
 LEFT JOIN course ON institution = institution.institutionid
 LEFT JOIN student ON course = course.courseid
